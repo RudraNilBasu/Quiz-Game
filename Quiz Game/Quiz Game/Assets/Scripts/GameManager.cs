8425dd6aba2f91762d165841aa21391db2a7848d
RudraNilBasu <rudra.nil.basu.1996@gmail.com>
2016-07-30 21:33:23 +0530

    Final UI operations

--- Quiz Game/Quiz Game/Assets/Scripts/GameManager.cs
+++ Quiz Game/Quiz Game/Assets/Scripts/GameManager.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
@@ -10,21 +12,73 @@ public class GameManager : MonoBehaviour {
 
 	private question currentQuestion;
 
+	[SerializeField]
+	private Text factText;
+
+	[SerializeField]
+	private float timeBetweenQuestions = 1.0f ;
+
+	[SerializeField]
+	private Text trueAnswerText, falseAnswerText;
+
+	[SerializeField]
+	private Animator animator;
+
 	void Start()
 	{
 		if (unansweredQuestions == null || unansweredQuestions.Count == 0) {
 			unansweredQuestions = questions.ToList<question> ();
 		}
 
-		GetRandomQuestion ();
-		Debug.Log (currentQuestion.fact + "  " + currentQuestion.isTrue);
+		SetCurrentQuestion ();
 	}
 
-	void GetRandomQuestion()
+	void SetCurrentQuestion()
 	{
 		int randomQuestionsIndex = Random.Range (0,unansweredQuestions.Count);
 		currentQuestion = questions [randomQuestionsIndex];
 
-		unansweredQuestions.RemoveAt (randomQuestionsIndex);
+		if (currentQuestion.isTrue) {
+			trueAnswerText.text = "CORRECT";
+			falseAnswerText.text = "WRONG";
+		} else {
+			trueAnswerText.text = "WRONG";
+			falseAnswerText.text = "CORRECT";
+		}
+
+		factText.text = currentQuestion.fact;
+
+
+	}
+
+	IEnumerator TranstionToNextQuestion()
+	{
+		unansweredQuestions.Remove (currentQuestion);
+
+		yield return new WaitForSeconds (1.0f );
+		SceneManager.LoadScene (SceneManager.GetActiveScene().buildIndex);
+	}
+
+	public void userSelectTrue()
+	{
+		animator.SetTrigger ("True");
+		if (currentQuestion.isTrue) {
+			Debug.Log ("Correct");
+		} else {
+			Debug.Log ("WRONG");
+		}
+		StartCoroutine (TranstionToNextQuestion());
+	}
+
+	public void userSelectFalse()
+	{
+		animator.SetTrigger ("False");
+		if (!currentQuestion.isTrue) {
+			Debug.Log ("Correct");
+		} else {
+			Debug.Log ("WRONG");
+		}
+
+		StartCoroutine (TranstionToNextQuestion());
 	}
 }
